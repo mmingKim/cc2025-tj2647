36ee6677b863a40b9d72f199fbdaf212dac43f71
mming-nyu <tj2647@nyu.edu>
2025-09-16 17:15:35 -0400

    week2

--- week2/sketch.js
+++ week2/sketch.js
@@ -3,8 +3,19 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
+  print(width,height)
 }
 
 function draw() {
-  
+background(0,200,100)
+
+fill(20, 300, 40,)
+noStroke()
+rect(100, 200, 30, 40)
+
+stroke(20 )
+strokeWeight(12)
+fill(20,40,300)
+ellipse(200,100,100,150)
+
 }
